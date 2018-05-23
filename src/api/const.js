1b9216c0a63f8c77656bbc0c7a026ebf1b7d759a
Vadim Patsev <vadim@smekalka.com>
2018-05-23 19:33:33 +0300

    Heartbeat for web

---
+++ src/api/const.js
@@ -0,0 +1,7 @@
+export const API_AUTH = '/auth';
+export const API_SIGNALS = '/signals';
+export const API_HEARTBEAT = '/heartbeat';
+export const API_EVENTS = '/events';
+export const API_BOOKINGS = '/bookings';
+export const API_SELF_ITINERARY = '/self/itinerary';
+export const API_SELF_ACTIVITIES = '/self/activities';
---
+++ src/api/index.js
@@ -0,0 +1,130 @@
+import axios from 'axios';
+import _ from 'lodash';
+
+export default class Api {
+    constructor({store, isNative}) {
+        this.isNative = isNative;
+        this.store = store;
+    }
+
+    _requestHelper = (method, {url, data, params, headers, timeout, cancelToken}) => {
+        const {baseUrl, authorization, userAgent, acceptLanguage, xHttpMethodOverride, locale, debug} = _.get(
+            this.store.getState(),
+            'terminal',
+            {}
+        );
+
+        axios.interceptors.request.use(config => {
+            if (debug && !_.get(config, 'skipLogs.request')) {
+                console.log(
+                    'SIMPOZIO_SDK API REQUEST: ',
+                    _.pick(config, ['data', 'url', 'timeout', 'method', 'headers'])
+                );
+            }
+
+            return _.assign({}, config, {
+                requestStartTime: Date.now()
+            });
+        });
+
+        // Store response timestamp and/or response time in response/error `config`
+        axios.interceptors.response.use(
+            response => {
+                const requestTime = Date.now() - response.config.requestStartTime;
+                const {result, status} = _.get(response, 'data', {});
+                if (status === 'ok') {
+                    if (debug && !_.get(response, 'config.skipLogs.response')) {
+                        console.log(
+                            `SIMPOZIO_SDK API RESPONSE: ${_.get(response, 'config.method')} ${_.get(
+                                response,
+                                'config.url'
+                            )}`,
+                            result
+                        );
+                    }
+                    return Promise.resolve({
+                        result: result,
+                        requestTime
+                    });
+                } else {
+                    if (debug && !_.get(response, 'config.skipLogs.error')) {
+                        console.log(
+                            `SIMPOZIO_SDK API ERROR: ${_.get(response, 'config.method')} ${_.get(
+                                response,
+                                'config.url'
+                            )}`,
+                            response
+                        );
+                    }
+                    return Promise.reject({result, status});
+                }
+            },
+            error => {
+                if (debug && !_.get(error, 'config.skipLogs.error')) {
+                    if (error.response) {
+                        console.log(
+                            `SIMPOZIO_SDK ERROR API: ${_.get(error, 'config.method')} ${_.get(
+                                error,
+                                'config.url'
+                            )} ${_.get(error, 'response.status')}`,
+                            _.get(error, 'response.data')
+                        );
+                    } else if (error.request) {
+                        console.log(
+                            `ERROR API TIMEOUT: ${_.get(error, 'config.method')} ${_.get(error, 'config.url')}`
+                        );
+                    } else {
+                        console.log(
+                            `ERROR API: ${_.get(error, 'config.method')} ${_.get(error, 'config.url')}`,
+                            _.get(error, 'message')
+                        );
+                    }
+                }
+            }
+        );
+
+        return axios({
+            url,
+            data,
+            method,
+            timeout,
+            baseUrl,
+            cancelToken,
+            headers: _.assign(
+                {},
+                {
+                    Date: moment.utc().format(`ddd, D MMM YYYY HH:mm:ss`) + ' GMT',
+                    Authorization: authorization,
+                    'User-Agent': userAgent,
+                    'Accept-Language': acceptLanguage,
+                    'X-HTTP-Method-Override': xHttpMethodOverride
+                },
+                headers
+            ),
+            params: _.assign({}, params, {
+                locale
+            })
+        });
+    };
+
+    get = ({url, data, params, headers, timeout, cancelToken}) => {
+        this._requestHelper('get', {url, data, params, headers, timeout, cancelToken});
+    };
+
+    post = ({url, data, params, headers, timeout, cancelToken}) => {
+        this._requestHelper('post', {url, data, params, headers, timeout, cancelToken});
+    };
+
+    delete = ({url, data, params, headers, timeout, cancelToken}) => {
+        this._requestHelper('delete', {url, data, params, headers, timeout, cancelToken});
+    };
+
+    put = ({url, data, params, headers, timeout, cancelToken}) => {
+        this._requestHelper('put', {url, data, params, headers, timeout, cancelToken});
+    };
+
+    makeCancelToken = () => {
+        const CancelToken = axios.CancelToken;
+        return CancelToken.source();
+    };
+}
