f49e7af99bc219820c16b962f33169fea0387b59
Vadim Patsev <vadim@smekalka.com>
2018-07-23 18:26:07 +0300

    Gulp build fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 const gulp = require('gulp');
 const shell = require('gulp-shell');
 
-gulp.task('default', () =>
+gulp.task('default', function() {
     gulp
         .src('package.json', {read: false})
-        .pipe(shell(['npx babel src -q --out-dir dist; npx webpack;'], {quiet: true}))
-);
-gulp.watch('src/**/*', ['default']);
+        .pipe(shell(['npx babel src -q --out-dir dist; npx webpack;'], {quiet: true}));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/**/*', ['default']);
+});
