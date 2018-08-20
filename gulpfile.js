fcafaea561f05d43afb63ccdaca9e334cbf6ca86
Vadim Patsev <vadim@smekalka.com>
2018-08-20 20:45:58 +0300

    build fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,33 @@
 const gulp = require('gulp');
-const shell = require('gulp-shell');
+const path = require('path');
+const babel = require('gulp-babel');
+const webpack = require('webpack-stream');
+const {babel: babelConf} = require('./package.json');
 
-gulp.task('default', () =>
+gulp.task('default', ['babel', 'webpack']);
+
+gulp.task('babel', () =>
     gulp
-        .src('package.json', {read: false})
-        .pipe(shell(["npx babel src -q --out-dir dist --ignore '**/*.test.js'; npx webpack;"], {quiet: true}))
+        .src(['./src/**/*.js', '!./src/**/*.test.js'])
+        .pipe(babel(babelConf))
+        .pipe(gulp.dest('dist'))
+);
+
+gulp.task('webpack', () =>
+    gulp
+        .src('./tools/example-init.js')
+        .pipe(
+            webpack({
+                entry: './tools/example-init.js',
+                output: {
+                    filename: 'init.js'
+                },
+                stats: {
+                    warnings: false
+                }
+            })
+        )
+        .pipe(gulp.dest('examples'))
 );
 
 gulp.task('watch', function() {
